7ed2fa35943caf4369f3618c657b3c3913c8fa56
Nicholas Raptis <nraptis@Nicholass-MacBook-Pro.local>
2025-02-16 10:41:40 -0500

    initial commit

---
+++ Tango/TangoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TangoApp.swift
+//  Tango
+//
+//  Created by Nicholas Raptis on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct TangoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TangoTests/TangoTests.swift
@@ -0,0 +1,16 @@
+//
+//  TangoTests.swift
+//  TangoTests
+//
+//  Created by Nicholas Raptis on 2/16/25.
+//
+
+import Testing
+
+struct TangoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
